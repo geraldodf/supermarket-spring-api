80948722fcd98ccba36695314c4e5eae86f24eb3
Daroz <geraldodarosfilho@hotmail.com>
2021-12-06 19:20:20 -0300

    Criando modelo de Produto.

---
+++ src/main/java/supermercado/api/Produto.java
@@ -0,0 +1,41 @@
+package supermercado.api;
+
+public class Produto {
+
+    private String descricao;
+    private Double preco;
+    private Long codigo;
+    private Long quantidade;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public Long getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Long quantidade) {
+        this.quantidade = quantidade;
+    }
+}
