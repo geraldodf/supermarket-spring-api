2c658d512735d71ad524c54fa8915be275b9ee57
Daroz <geraldodarosfilho@hotmail.com>
2021-12-08 10:46:15 -0300

    Completando método GET e começando o método POST.

--- src/main/java/supermercado/api/resources/ProdutoResource.java
+++ src/main/java/supermercado/api/resources/ProdutoResource.java
@@ -2,6 +2,7 @@ package supermercado.api.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import supermercado.api.models.Produto;
@@ -21,5 +22,11 @@ public class ProdutoResource {
     public ArrayList<Produto> pegarTodosProdutos () {
         return produtoService.pegarTodosProdutos();
     }
+
+    @PostMapping
+    public void adicionarProduto(){
+        produtoService.adicionarProduto();
+    }
+
 
 }
--- src/main/java/supermercado/api/services/ProdutoService.java
+++ src/main/java/supermercado/api/services/ProdutoService.java
@@ -1,11 +1,21 @@
 package supermercado.api.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import supermercado.api.models.Produto;
+import supermercado.api.repositories.ProdutoRepository;
 
 import java.util.ArrayList;
 
 
 public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
     public ArrayList<Produto> pegarTodosProdutos() {
+        return (ArrayList<Produto>) produtoRepository.findAll();
+    }
+
+    public void adicionarProduto() {
     }
 }
