8ab79e445e7042b464519fb1d94122034281987f
Geraldo Daros <78937716+geraldodf@users.noreply.github.com>
2023-01-15 14:47:19 +0100

    Renaming of RoleService to English.

--- src/main/java/br/com/supermarket/resources/RouleResource.java
+++ src/main/java/br/com/supermarket/resources/RouleResource.java
@@ -1,7 +1,7 @@
 package br.com.supermarket.resources;
 
 import br.com.supermarket.models.Role;
-import br.com.supermarket.services.CargoService;
+import br.com.supermarket.services.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
@@ -11,36 +11,36 @@ import java.util.ArrayList;
 public class RouleResource {
 
     @Autowired
-    private CargoService rouleService;
+    private RoleService rouleService;
 
     @GetMapping
     public ArrayList<Role> getAllRoules() {
-        return rouleService.pegarTodosCargos();
+        return rouleService.getAllRoles();
     }
 
     @GetMapping("/{id}")
     public Role getRouleById(@PathVariable("id") Long id) throws Exception {
-        return rouleService.pegarCargoPeloId(id);
+        return rouleService.getRoleById(id);
     }
 
     @GetMapping("/name")
     public ArrayList<Role> getRouleByName(@RequestParam(name = "name") String name) throws Exception {
-        return rouleService.pegarCargoPeloNome(name);
+        return rouleService.getRolesByName(name);
     }
 
     @PostMapping
     public Role createRoule(@RequestBody Role role) throws Exception {
-        return rouleService.criarCargo(role);
+        return rouleService.createRole(role);
     }
 
     @PutMapping("/{id}")
     public Role updateRoule(@PathVariable("id") Long id, @RequestBody Role role) throws Exception {
-        return rouleService.atualizarCargo(id, role);
+        return rouleService.updateRole(id, role);
     }
 
     @DeleteMapping("/{id}")
     public void deleteRoleById(@PathVariable("id") Long id) throws Exception {
-        rouleService.excluirCargo(id);
+        rouleService.deleteRole(id);
     }
 
 }
---
+++ src/main/java/br/com/supermarket/services/RoleService.java
@@ -0,0 +1,90 @@
+package br.com.supermarket.services;
+
+import br.com.supermarket.models.Role;
+import br.com.supermarket.repositories.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Service
+public class RoleService {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    public ArrayList<Role> getAllRoles() {
+        return (ArrayList<Role>) roleRepository.findAll();
+    }
+
+    public Role getRoleById(Long id) throws Exception {
+
+        if (id == null)
+            throw new Exception("The role is is null.");
+
+        Optional<Role> optionalRole = null;
+        try {
+            optionalRole = roleRepository.findById(id);
+        } catch (Exception e) {
+            throw new Exception("Error when searching role by ID");
+        }
+        return optionalRole.get();
+    }
+
+    public ArrayList<Role> getRolesByName(String name) throws Exception {
+        if (name == null || "".equals(name)) {
+            throw new Exception("Enter a search name.");
+        }
+        try {
+            roleRepository.searchByName(name);
+        } catch (Exception e) {
+            throw new Exception("Error searching role by name");
+        }
+        return roleRepository.searchByName(name);
+    }
+
+    public Role createRole(Role role) throws Exception {
+        if (role.getName() == null || role.getName().length() <= 3)
+            throw new Exception("Role name must be specified and be at least 3 characters long.");
+        try {
+            roleRepository.save(role);
+        } catch (Exception e) {
+            throw new Exception("Error saving role in database.");
+        }
+        return role;
+    }
+
+    public Role updateRole(Long id, Role role) throws Exception {
+        Optional<Role> optionalRole;
+        try {
+            optionalRole = roleRepository.findById(id);
+        } catch (Exception e) {
+            throw new Exception("Error searching for role to be changed!");
+        }
+
+        Role roleUpdating = optionalRole.get();
+
+        if (role.getName() == null || role.getName().length() <= 3)
+            throw new Exception("Role name must be specified and be at least 3 characters long");
+
+        roleUpdating.setName(role.getName());
+
+        try {
+            roleRepository.save(roleUpdating);
+        } catch (Exception e) {
+            throw new Exception("Error saving the new role in the database!");
+        }
+
+        return roleUpdating;
+    }
+
+    public void deleteRole(Long id) throws Exception {
+
+        try {
+            roleRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new Exception("Role has not been deleted.!");
+        }
+    }
+}
